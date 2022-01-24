2b30d34d2ec4779ea92cb5872900629915d45852
Austin Thaldorf-Huelsbeck <75189212+austinthaldorfhuelsbeck@users.noreply.github.com>
2022-01-24 10:01:04 -0800

    categories view setup + bootstrap

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -5,10 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddRazorPages();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 
 
